95a3e6e91f42a108842e563b9fe6caf9d8264f95
arpita.sristy <arpita.sristy18@gmail.com>
2022-01-02 17:46:53 +0600

    search option done

--- client-site/src/App.js
+++ client-site/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
-import JSONDATA from './services.json'
+import JSONDATA from './services.json';
+import {useState} from 'react';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
     <div className="App">
-      <input type='text' placeholder='Search...' />
-      {JSONDATA.map((val, key) => {
-        return <div> {val.name} </div>
+      <input type='text' placeholder='Search...' onChange={event => {setSearchTerm(event.target.value)}} />
+      {JSONDATA.filter((val) => {
+        if (searchTerm == ""){
+          return val
+        } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return val
+        }
+      }).map((val, key) => {
+        return (
+          <div className='user' key={key}>
+           <p>{val.name}</p>
+           <p>{val.offer}</p>
+           <p>{val.description}</p> 
+          </div>
+        );
       })}
     </div>
   );
