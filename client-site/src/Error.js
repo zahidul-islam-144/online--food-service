4e835d19dc48903bc6912c917bbf177073266536
arpita.sristy <arpita.sristy18@gmail.com>
2022-01-04 01:05:17 +0600

    error

---
+++ client-site/src/Error.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+      <img
+        src="https://vectorforfree.com/wp-content/uploads/2020/03/Error_404_VectroForFree.png"
+        className="mt-8 w-2/4 mx-auto"
+        alt=""
+        srcset=""
+      />
+    </div>
+    );
+};
+
+export default Error;
